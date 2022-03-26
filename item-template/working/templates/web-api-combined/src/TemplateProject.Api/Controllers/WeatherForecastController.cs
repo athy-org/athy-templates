aa4808f8a4721310d163b8d5ac8267ae0149d292
JPThorne <thorne.jean.paul@gmail.com>
2022-03-26 15:12:39 +0200

    Add combined template example for user-code

---
+++ item-template/working/templates/web-api-combined/src/TemplateProject.Api/Controllers/WeatherForecastController.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace TemplateProject.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+[Produces("application/json")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    /// <summary>
+    /// EndpointDescription
+    /// </summary>
+    /// <param name="number"></param>
+    /// <returns></returns>
+    [HttpGet("{number}")]
+    [ProducesResponseType(typeof(IEnumerable<WeatherForecast>), (200), "application/json")]
+    public IEnumerable<WeatherForecast> Get([FromRoute] int number)
+    {
+        return Enumerable.Range(1, number).Select(index => new WeatherForecast
+        {
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+}
